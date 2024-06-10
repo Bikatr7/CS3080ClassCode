08dd12a53da933458447002ddb2e206b058509ea
Bikatr7 <Bikatr7@proton.me>
2024-06-10 15:16:06 -0600

    added code for assignment 1

---
+++ Proof_of_Installation/main.py
@@ -0,0 +1,9 @@
+text_block = """
+Name: Kaden Bilyeu
+Major: Computer Science
+Year: 3 (Rising Junior)
+What you want to learn: want to learn Django and websockets, since I already know Flask and FastAPI as well as the rest of the stuff this class teaches.
+What has helped you learn to program in the past: As for what helps to learn, I think that the best way to learn is to have something you want to make, and figure out how to make it.
+"""
+
+print(text_block)
