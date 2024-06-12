388f0938f5837f81b58be311e2d2c37e5553bc86
Bikatr7 <Bikatr7@proton.me>
2024-06-12 13:49:19 -0600

    rearranged things, added initial homework 1 code

---
+++ Homework/intro_to_function_homework_1/homework1.py
@@ -0,0 +1,90 @@
+## built-in libraries
+import typing
+
+##-------------------start-of-sum()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def sum(first_number:typing.Union[int, float], second_number:typing.Union[int, float]) -> int:
+
+    """
+    
+    This function takes two numbers as input and returns the sum of the two numbers.
+
+    Parameters:
+    first_number (int | float): The first number to be added.
+    second_number (int | float): The second number to be added.
+
+    Returns:
+    int: The sum of the two numbers.
+
+    """
+    
+    first_number = int(first_number)
+    second_number = int(second_number)
+
+    return first_number + second_number
+
+##-------------------start-of-sub()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def sub(first_number:typing.Union[int, float], second_number:typing.Union[int, float]) -> int:
+
+    """
+    
+    This function takes two numbers as input and returns the difference of the two numbers.
+
+    Parameters:
+    first_number (int | float): The first number to be subtracted.
+    second_number (int | float): The second number to be subtracted.
+
+    Returns:
+    int: The difference of the two numbers.
+
+    """
+    
+    first_number = int(first_number)
+    second_number = int(second_number)
+
+    return first_number - second_number
+
+##-------------------start-of-mul()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def mul(first_number:typing.Union[int, float], second_number:typing.Union[int, float]) -> int:
+
+    """
+    
+    This function takes two numbers as input and returns the product of the two numbers.
+
+    Parameters:
+    first_number (int | float): The first number to be multiplied.
+    second_number (int | float): The second number to be multiplied.
+
+    Returns:
+    int: The product of the two numbers.
+
+    """
+    
+    first_number = int(first_number)
+    second_number = int(second_number)
+
+    return first_number * second_number
+
+##-------------------start-of-div()--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def div(first_number:typing.Union[int, float], second_number:typing.Union[int, float]) -> float:
+
+    """
+    
+    This function takes two numbers as input and returns the division of the two numbers.
+
+    Parameters:
+    first_number (int | float): The first number to be divided.
+    second_number (int | float): The second number to be divided.
+
+    Returns:
+    float: The division of the two numbers.
+
+    """
+    
+    first_number = int(first_number)
+    second_number = int(second_number)
+
+    return first_number / second_number
